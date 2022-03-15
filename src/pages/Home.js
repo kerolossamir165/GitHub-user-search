fe2d3cd20aa78a2e66cd76ea1ccd17b712fa581b
Kerolos Samir <Kerolos.Samir@g2k-group.com>
2022-03-15 18:15:46 +0200

    'update-redirects'

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { useHistory } from "react-router-dom";
 import Pagination from "../components/Pagination/Pagination";
 import SearchForm from "../components/Search/SearchForm";
